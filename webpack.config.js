3be162e1ef2603d99125041878f36cbca9aa09c9
Michal Malý <michal.maly@ibt.cas.cz>
2020-08-04 13:53:41 +0200

    Add DNATCO application

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ const tests = [
 module.exports = [
     createApp('viewer', 'molstar'),
     createApp('docking-viewer', 'molstar'),
+    createApp('dnatco', 'molstar'),
     ...examples.map(createExample),
     ...tests.map(createBrowserTest)
 ];
