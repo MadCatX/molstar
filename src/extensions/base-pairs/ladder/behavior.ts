c3ef5ce8c077be86793cecaf9a4723ef4552e78a
Michal Malý <madcatxster@devoid-pointer.net>
2025-07-19 22:51:40 +0200

    Add simplified color theme for base pairs ladder

--- src/extensions/base-pairs/ladder/behavior.ts
+++ src/extensions/base-pairs/ladder/behavior.ts
@@ -1,4 +1,4 @@
-import { BasePairsLadderColorThemeProvider } from './color';
+import { BasePairsLadderSimpleColorThemeProvider } from './color';
 import { BasePairsLadderProvider } from './property';
 import { BasePairsLadderRepresentationProvider } from './representation';
 import { BasePairsLadderTypes } from './types';
@@ -34,7 +34,7 @@ export const BasePairsLadderPreset = StructureRepresentationPresetProvider({
 
         let ladderRepr;
         if (representations)
-            ladderRepr = builder.buildRepresentation(update, ladder, { type: BasePairsLadderRepresentationProvider, typeParams, color: BasePairsLadderColorThemeProvider }, { tag: 'base-pairs-ladder' });
+            ladderRepr = builder.buildRepresentation(update, ladder, { type: BasePairsLadderRepresentationProvider, typeParams, color: BasePairsLadderSimpleColorThemeProvider }, { tag: 'base-pairs-ladder' });
 
         await update.commit({ revertOnError: true });
         return { components: { ...components, ladder }, representations: { ...representations, ladderRepr } };
